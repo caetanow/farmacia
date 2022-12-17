4010564e5193cb3e241cf41b47cd6cf88f971d18
Willian de Jesus Caetano <willian.caetano@mercadolivre.com>
2022-12-17 18:07:30 -0300

    incluido @Repository na camada repository

--- src/main/java/com/generation/farmacia/repository/CategoriaRepository.java
+++ src/main/java/com/generation/farmacia/repository/CategoriaRepository.java
@@ -3,9 +3,11 @@ package com.generation.farmacia.repository;
 import com.generation.farmacia.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
     public List<Categoria> findAllByMarcaContainingIgnoreCase(@Param("marca") String marca);
 }
--- src/main/java/com/generation/farmacia/repository/ProdutoRepository.java
+++ src/main/java/com/generation/farmacia/repository/ProdutoRepository.java
@@ -4,9 +4,11 @@ import com.generation.farmacia.model.Categoria;
 import com.generation.farmacia.model.Produtos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produtos, Long>{
     public List<Produtos> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
